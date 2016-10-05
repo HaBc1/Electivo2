832ddacab11163a089666a89f6f748c736b4dc50
joeldcm <joeldcm@gmail.com>
2016-10-05 15:17:26 -0500

    Create listarcurso.php

---
+++ Semana7/listarcurso.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<fieldset>
+    <legend>
+        :Lista de cursos
+    </legend>
+    <table border="1">
+        <tr>
+        <th>Codigo:</th>
+        <th>Nombre:</th>
+        <th>Horas:</th>
+        <th>Credito Horas:</th>
+        </tr>
+        <tr>
+            <td>01</td>
+            <td>matematicas</td>
+            <td>03</td>
+            <td>4</td>
+        </tr>
+    </table>
+    
+</fieldset>
